c61b61401ad2da8fadd9355d287bbeaf9d77ac91
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-03-15 08:47:31 +0530

    Added basic input functionalities, basic character animations and a simple stage

---
+++ Player.gd
@@ -0,0 +1,79 @@
+extends KinematicBody2D
+
+export(int) var speed = 30
+
+const GRAVITY = 10
+const JUMP_FORCE = -250
+const FLOOR = Vector2(0, -1)
+
+const REGULAR_BULLET = preload("res://RegularBullet.tscn")
+
+var velocity = Vector2()
+var is_on_ground = false
+var is_attacking = false
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	
+#	HORIZONTAL MOVEMENT
+	if Input.is_action_pressed("ui_right"):
+		if !is_attacking or !is_on_floor():
+			velocity.x = speed
+			if !is_attacking:
+				$Player_Anim.play("Run")
+				$Player_Anim.flip_h = false
+				if sign($ShootPoint.position.x)  == -1:
+					$ShootPoint.position.x *= -1
+	elif Input.is_action_pressed("ui_left"):
+		if !is_attacking or !is_on_floor():
+			velocity.x = -speed
+			if !is_attacking:
+				$Player_Anim.play("Run")
+				$Player_Anim.flip_h = true
+				if sign($ShootPoint.position.x) == 1:
+					$ShootPoint.position.x *= -1
+	else:
+		velocity.x = 0
+		if is_on_ground and !is_attacking:
+			$Player_Anim.play("Idle")
+	
+#	VERTICAL MOVEMENT
+	if Input.is_action_pressed("ui_up") and is_on_ground:
+		if !is_attacking:
+			velocity.y = JUMP_FORCE
+			is_on_ground = false
+		
+#	SHOOT
+	if Input.is_action_just_pressed("ui_select") and !is_attacking:
+		if is_on_floor():
+			velocity.x = 0
+		is_attacking = true
+		$Player_Anim.play("Shoot")
+		var weapon_projectile = REGULAR_BULLET.instance()
+		weapon_projectile.set_direction(sign($ShootPoint.position.x))
+		get_parent().add_child(weapon_projectile)
+		weapon_projectile.position = $ShootPoint.global_position
+	
+#	PROCESSING GRAVITY
+	velocity.y += GRAVITY
+	
+#	IS PLAYER ON FLOOR?
+	if is_on_floor():
+		if !is_on_ground:
+			is_attacking = false
+		is_on_ground = true
+	else:
+		if !is_attacking:
+			is_on_ground = false
+			if velocity.y < 0:
+				$Player_Anim.play("Jump")
+			else:
+				$Player_Anim.play("Fall")
+		
+	velocity = move_and_slide(velocity, FLOOR)
+
+#IS THE ANIMATION DONE?
+func _on_Player_Anim_animation_finished():
+	is_attacking = false
---
+++ RegularBullet.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+export(int) var speed = 200
+var velocity = Vector2()
+var direction = 1
+
+func _ready():
+	pass # Replace with function body.
+
+#Change Direction
+func set_direction(dir):
+	direction = dir
+	if direction == -1:
+		$Bullet_Anim.flip_h = true
+	else:
+		$Bullet_Anim.flip_h = false
+
+func _physics_process(delta):
+	velocity.x = speed * delta * direction
+	translate(velocity)
+	$Bullet_Anim.play("BulletFired")
+
+
+#RELEASE IF OUT OF SCREEN
+func _on_Bullet_Visibility_screen_exited():
+	queue_free()
+
+#RELEASE IF COLLIDE
+func _on_RegularBullet_body_entered(body):
+	queue_free()
